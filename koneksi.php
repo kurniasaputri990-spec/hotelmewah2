3fa21a02b16363c1096f060f403e1c716acd5875
kurniasaputri990-spec <kurniasaputri990@gmail.com>
2026-06-13 06:36:36 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db   = 'pemesanan_hotel2';
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$user = getenv('MYSQLUSER') ?: 'root';
+$pass = getenv('MYSQLPASSWORD') ?: '';
+$db   = getenv('MYSQLDATABASE') ?: 'pemesanan_hotel';
+$port = getenv('MYSQLPORT') ?: 3306;
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
